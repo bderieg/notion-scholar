1f46f5364f138a777d6cd565da96c6e7a6009946
Benjamin J. Derieg <96968686+bderieg@users.noreply.github.com>
2025-05-19 09:29:48 -0600

    Remove { and } from Notion entries

--- notion_scholar/bibtex.py
+++ notion_scholar/bibtex.py
@@ -99,7 +99,7 @@ def get_publication_list(bib_database: BibDatabase) -> List[Publication]:
         publications.append(
             Publication(
                 key=entry.get('ID', ''),
-                title=entry.get('title', ''),
+                title=entry.get('title', '').replace('{','').replace('}',''),
                 authors=entry.get('author', '').replace('\n', ' '),
                 year=int(entry['year']) if 'year' in entry.keys() else None,
                 journal=entry.get('journal', ''),
